0322ebc11351198bc3e994d105f9f062577c2ae8
jarmal937 <66896619+jarmal937@users.noreply.github.com>
2025-05-03 16:44:39 +0530

    Delete Learning_templates directory

--- Learning_templates/basicapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from basicapp import views
-app_name="basicapp"
-
-urlpatterns=[
-    path('relative/',views.relative_urls_templates,name="relative_urls_templates"),
-    path('other/',views.other,name="other"),
- ]
--- Learning_templates/basicapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    return render(request,"basicapp/index.html")
-def other(request):
-    return render(request,"basicapp/other.html")
-def relative_urls_templates(request):
-    return render(request,"basicapp/relative_urls_templates.html")
